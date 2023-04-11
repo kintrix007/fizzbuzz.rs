b6ab301b91a29975075a5def4a59d33dfa022239
kintrix007 <boldizsar02@gmail.com>
2023-04-12 00:49:42 +0200

    Added comment to clarify for myself that I should probably just print and exit

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ fn main() {
 
     let stdin = stdin();
 
+    // Note to self: this is not how you print an error and gracefully exit.
     let n = stdin.lock().lines().nth(0).expect("Must not be empty.");
     let n = n.expect("I have genuinely no clue what could go wrong here.");
     let n = n.parse::<i32>().expect("Please provide a whole number.");
